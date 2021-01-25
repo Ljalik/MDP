c1be46277337fd4684c31cb860843c6cc5e47b85
opilane <opilane@HP-CZC6297B8H>
2021-01-25 14:13:07 +0200

    Add project files.

---
+++ marki_smirnov/marki_smirnov/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace marki_smirnov
+{
+    public  class mastermenuitem
+    {
+        public string Text { get; set; }
+        public string Detail { get; set; }
+        public string ImagePath { get; set; }
+        public Type TargetPage { get; set;
+    }
+}
